321db7cf29e21458c947848f7a01c39f813c90ca
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-12-22 19:36:49 +0530

    api updated

--- src/services/api.js
+++ src/services/api.js
@@ -46,8 +46,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-// const API_BASE_URL = 'https://yescityreasearchdashboard.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://yescityreasearchdashboard.onrender.com/api';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
